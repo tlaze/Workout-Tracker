981507d9a482e63d24e6ad5d7ed9c8f4d11b033b
tlaze <tomlazore@gmail.com>
2021-11-09 10:48:21 -0500

    updated mongoose.connect

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const db = require('../models');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 const workoutSeed = [
