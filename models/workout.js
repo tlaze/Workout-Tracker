23b0839bb9410ba1760c52555734b51a76e0a90e
tlaze <tomlazore@gmail.com>
2021-11-08 22:47:52 -0500

    updated workout model

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    
+    exercise: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+        },
+    ],
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
