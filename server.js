45e9d7fec67f273fb6aeb7db8d38831c765e4094
tlaze <tomlazore@gmail.com>
2021-11-08 22:38:04 -0500

    homeRoutes updated

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
-
-const PORT = process.env.PORt || 3000;
+const homeRoutes = require('./routes/homeRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
 });
+
+app.use(homeRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
